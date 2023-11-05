ae80113270cbecc51fad52f4a706b08708af0e9d
mk812y <mk812y@gmail.com>
2023-11-05 20:31:38 +0100

    Initial Commit

---
+++ easyRačun/easyRac_unApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyRac_unApp.swift
+//  easyRačun
+//
+//  Created by Михаил Куприянов on 5.11.23..
+//
+
+import SwiftUI
+
+@main
+struct easyRac_unApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
