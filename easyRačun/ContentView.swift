726eb2062cedfbfcf1b6f15e19d8f1cf0886ee43
mk812y <mk812y@gmail.com>
2023-11-06 19:13:49 +0100

    https://github.com/twostraws/CodeScanner

--- easyRačun/ContentView.swift
+++ easyRačun/ContentView.swift
@@ -6,31 +6,37 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    @State private var isShowingScanner = false
     @State private var racunData: [Racun] = []
-
-        var body: some View {
-            List(racunData.indices, id: \.self) { index in
-                let racun = racunData[index]
-                VStack(alignment: .leading) {
-                    Text("Date and Time: \(racun.DateTime)")
-                        .font(.title3)
-                    ForEach(racun.Items, id: \.self) { item in
-                        Text("Name: \(item.Name)")
-                        Text("Quantity: \(item.Quantity)")
-                        Text("Total: \(item.Total)")
-                        Divider()
-                    }
+    
+    var body: some View {
+        List(racunData.indices, id: \.self) { index in
+            let racun = racunData[index]
+            VStack(alignment: .leading) {
+                Text("Date and Time: \(racun.DateTime)")
+                    .font(.title3)
+                ForEach(racun.Items, id: \.self) { item in
+                    Text("Name: \(item.Name)")
+                    Text("Quantity: \(item.Quantity)")
+                    Text("Total: \(item.Total)")
+                    Divider()
                 }
-                .padding(.bottom, 30)
             }
-            .onAppear {
-                DataModel().fetchData { data in
-                    self.racunData = data
-                }
+            .padding(.bottom, 30)
+        }
+        .onAppear {
+            DataModel().fetchData { data in
+                self.racunData = data
             }
         }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+    }
 }
 
 
